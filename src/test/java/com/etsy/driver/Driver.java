c60bd4d0008f5c06e3595306a4dd237e648a4256
Karen <karend.539@gmail.com>
2019-12-04 00:35:44 -0500

    Update driver

--- src/test/java/com/etsy/driver/Driver.java
+++ src/test/java/com/etsy/driver/Driver.java
@@ -1,5 +1,6 @@
 package com.etsy.driver;
 
+import com.etsy.utils.Constants;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,5 +12,12 @@ public class Driver {
     public static void initDriver(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.get(Constants.Url.URL);
+
+//        WebDriverManager.firefoxdriver().setup();
+//        FirefoxOptions optionsFirefox = new FirefoxOptions();
+//        optionsFirefox.addArguments("--kiosk");
+//        driver = new FirefoxDriver(optionsFirefox);
+//        driver.get(Constants.Url.URL);
     }
 }
--- src/test/java/com/etsy/pages/account/CreateAccount.java
+++ src/test/java/com/etsy/pages/account/CreateAccount.java
@@ -2,12 +2,9 @@ package com.etsy.pages.account;
 
 import com.etsy.driver.Driver;
 import com.etsy.utils.WebElementsUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.yecht.Data;
 
 public class CreateAccount {
 
