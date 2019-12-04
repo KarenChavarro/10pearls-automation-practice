ec6ff0c414a09612b73d2cab35f4bed039b11b37
KarenChavarro <32947826+KarenChavarro@users.noreply.github.com>
2019-12-04 00:10:46 -0500

    Update Driver.java

--- src/test/java/com/etsy/driver/Driver.java
+++ src/test/java/com/etsy/driver/Driver.java
@@ -1,10 +1,8 @@
 package com.etsy.driver;
 
-import com.etsy.utils.Constants;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Driver {
 
