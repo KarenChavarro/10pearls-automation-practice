0697f41048a508f6b8cf632830c88c206c05bbf1
KarenChavarro <32947826+KarenChavarro@users.noreply.github.com>
2019-12-05 16:35:43 -0500

    Update CreateAccountTest.java

--- src/test/java/com/etsy/runners/CreateAccountTest.java
+++ src/test/java/com/etsy/runners/CreateAccountTest.java
@@ -4,9 +4,9 @@ import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features= {"src/test/resources/features/CreateAccount.feature"},
-        glue = {"com.etsy.stepdefinitions.account"}
-)
+// @RunWith(CucumberWithSerenity.class)
+// @CucumberOptions(features= {"src/test/resources/features/CreateAccount.feature"},
+//         glue = {"com.etsy.stepdefinitions.account"}
+// )
 public class CreateAccountTest {
 }
